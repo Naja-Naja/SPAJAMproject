576ed16835409bbfc7b0b537e0b039721d3c9567
618knot <knot618@gmail.com>
2022-10-23 03:23:58 +0900

    InputFormからgameproperties.calorieに値を入れられるように変更

---
+++ Assets/Project/Script/CalorieValueCtrl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalorieValueCtrl : MonoBehaviour
+{
+    public Text text;
+    public GameProperties gameProperties;
+    [SerializeField] private InputField TestInputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void GetText()
+    {
+        // 範囲外の場合gameProperties.calorieの値を変えることはできたが、InputFormの文字の方を変えられなかった。
+        text = text.GetComponent<Text>();
+        gameProperties.calorie = int.Parse(text.text);
+        if(gameProperties.calorie > 3000)
+        {
+            gameProperties.calorie = 3000; 
+            TestInputField.text = gameProperties.calorie.ToString();
+            //text.text = gameProperties.calorie.ToString();
+        }
+        else if(gameProperties.calorie <= 0)
+        {
+            gameProperties.calorie = 60;
+            TestInputField.text = gameProperties.calorie.ToString();
+            //text.text = gameProperties.calorie.ToString();
+        }
+        
+    }
+    void Update()
+    {
+        //Debug.Log(text.text);
+        //Debug.Log(gameProperties.calorie);
+
+    }
+}
