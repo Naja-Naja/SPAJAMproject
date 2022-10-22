1d9b3691e5acb450028a876bc267ae4ca59079ed
OHMORIYUSUKE <omoriyusuke@gmail.com>
2022-10-22 17:26:55 +0900

    誤差修正

--- Assets/Project/Script/Utils.cs
+++ Assets/Project/Script/Utils.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 
 public class Utils : MonoBehaviour
 {
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     var test = this.getAntCountPerSec(10, 100);
-    //     UnityEngine.Debug.Log(test);
-    //     for (int i = 0; i < test.Count; i++)
-    //     {
-    //         UnityEngine.Debug.Log(test[i]);
-    //     }
-    // }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 150匹が X 分を60分割して1分割あたりで出現する数
+        var test = this.getAntCountPerSec(60, 1000);
+        UnityEngine.Debug.Log(test);
+        double sum = 0;
+        for (int i = 0; i < test.Count; i++)
+        {
+            UnityEngine.Debug.Log(test[i]);
+            sum += test[i];
+        }
+        UnityEngine.Debug.Log(sum);
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
+    // Update is called once per frame
+    void Update()
+    {
 
-    // }
+    }
 
     List<float> getAntCountPerSec(int divideCount, int antCount)
     {
@@ -28,23 +32,20 @@ public class Utils : MonoBehaviour
         {
             throw new IndexOutOfRangeException("divideCountは1以上です。");
         }
-        // 傾き調整変数 2
-        int TILT_PARAM = 2;
-        // 最低出現数
-        int MIN_ANT_COUNT = 1;
         // 一定に出現擦る場合の出現数
-        float CONSTANT_NUM = antCount / divideCount;
+        double CONSTANT_NUM = antCount / divideCount;
 
-        float DELTA_Y = CONSTANT_NUM - MIN_ANT_COUNT;
-        float DELTA_X = divideCount / TILT_PARAM;
-        float TILT = DELTA_Y / DELTA_X;
+        double DELTA_Y = CONSTANT_NUM;
+        double DELTA_X = divideCount / 2;
+        double TILT = DELTA_Y / DELTA_X;
 
         // 1秒間に出現する数
         var result = new List<float>();
         for (int i = 0; i < divideCount; i++)
         {
-            float n = TILT * i + MIN_ANT_COUNT;
-            result.Add(n);
+            double n = TILT * i;
+            float n1 = (float)Math.Ceiling(n);
+            result.Add(n1);
         }
         return result;
     }
