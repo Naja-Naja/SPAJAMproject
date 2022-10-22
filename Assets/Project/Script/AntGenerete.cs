245dd5b6bff896fb5caa1a73ed2ef6127d1cd8ef
Naja-Naja <latimeria.h11@gmail.com>
2022-10-22 16:43:30 +0900

    アリ生成頻度の制御と位置の算出処理追加

---
+++ Assets/Project/Script/AntGenerete.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntGenerete : MonoBehaviour
+{
+    private List<float> timeList = new List<float>();
+    [SerializeField]int split;
+    [SerializeField] GameProperties properties;
+    [SerializeField] GameObject ant;
+    [SerializeField] GameObject player;
+    private void Start()
+    {
+        //floatlist‚ð“¾‚é
+        timeList.Add(0.5f);
+        StartCoroutine("generate");
+    }
+    IEnumerator generate()
+    {
+        float spawnrate = 0;
+        foreach (var item in timeList)
+        {
+            for (int i = 0; i < 60/split; i++)
+            {
+                spawnrate = spawnrate + item;
+                while (spawnrate > 1)
+                {
+                    //Instantiate()
+                    Debug.Log("generateant");
+                    spawnrate--;
+                }
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        Debug.Log("gameend");
+    }
+    Vector3 generatePos()
+    {
+        int random = Random.Range(-1, 1);
+        if (random == 0) { random = 1; }
+        float rnd = Random.Range(-3f, 3f);
+        var tmp = new Vector3(rnd, random*6, 0);
+        return tmp;
+    }
+}
