51344a2fa2c0a2fc4d3f0d5e5f76d5f0404dcee8
Naja-Naja <latimeria.h11@gmail.com>
2022-10-22 14:26:03 +0900

    GamePropertyに変数を追加

--- Assets/Project/Script/template/GameProperties.cs
+++ Assets/Project/Script/template/GameProperties.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum GameVisualMode
+{
+    spanyan,
+    ant
+}
+public enum GameMode
+{
+    play,
+    result
+}
 [CreateAssetMenu(fileName = "GameProperties", menuName = "GameProperties")]
 public class GameProperties : ScriptableObject
 {
     public int score;
+    public int calorie;
+    public GameVisualMode visualmode;
+    public GameMode mode;
+    public int HP;
 }
