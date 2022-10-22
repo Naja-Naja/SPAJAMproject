6723757cea0f50e21a05946db248f87c6e1aed7a
Naja-Naja <latimeria.h11@gmail.com>
2022-10-23 02:59:13 +0900

    リザルト画面への遷移実装

--- Assets/Project/Script/GameManager.cs
+++ Assets/Project/Script/GameManager.cs
@@ -7,13 +7,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameProperties properties;
+    [SerializeField] AntGenerete generete;
     int maxHP = 100;
+    public bool gameend = false;
+    [SerializeField] GameObject resultWindow;
     private void Start()
     {
         properties.HP = maxHP;
+        generete.CountStart();
+        gameend = false;
     }
     private void Update()
     {
         properties.score = properties.calorie * (properties.HP / maxHP);
+        if (gameend == true)
+        {
+            Instantiate(resultWindow, new Vector3(0, 0, 0), Quaternion.identity);
+        }
     }
 }
